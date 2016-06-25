778843927b695391f791a46c5a94695635425efb
Roaming <rangerroaming@gmail.com>
2016-06-25 17:29:43 +0530

    A loopback FTDI interface which echos onto the terminal whatever has been sent to it via minicom or any other serial terminal program via the ftdi_sio driver

---
+++ examples/loopback_ftdi/descriptors.c
@@ -0,0 +1,179 @@
+/*
+The descriptors below setup an FTDI device with a VID 
+of 0x0403, and PID of 0x6010.In this particular case
+endpoint 1 is used to transfer data in and out of the device
+since it can be easily accessed by the CPU and is not used
+for other purposes(Typically used for transferring small amounts
+of data which is what we need)
+lsusb -v will show:
+vbDevice Descriptor:
+  bLength                18
+  bDescriptorType         1
+  bcdUSB               2.00
+  bDeviceClass          255 Vendor Specific Class
+  bDeviceSubClass       255 Vendor Specific Subclass
+  bDeviceProtocol       255 Vendor Specific Protocol
+  bMaxPacketSize0        64
+  idVendor           0x0403 Future Technology Devices International, Ltd
+  idProduct          0x6010 FT2232C Dual USB-UART/FIFO IC
+  bcdDevice            0.01
+  iManufacturer           1 
+  iProduct                2 
+  iSerial                 0 
+  bNumConfigurations      1
+  Configuration Descriptor:
+    bLength                 9
+    bDescriptorType         2
+    wTotalLength           32
+    bNumInterfaces          1
+    bConfigurationValue     1
+    iConfiguration          0 
+    bmAttributes         0x80
+      (Bus Powered)
+    MaxPower              100mA
+    Interface Descriptor:
+      bLength                 9
+      bDescriptorType         4
+      bInterfaceNumber        0
+      bAlternateSetting       0
+      bNumEndpoints           2
+      bInterfaceClass       255 Vendor Specific Class
+      bInterfaceSubClass    255 Vendor Specific Subclass
+      bInterfaceProtocol    255 Vendor Specific Protocol
+      iInterface              3 
+      Endpoint Descriptor:
+        bLength                 7
+        bDescriptorType         5
+        bEndpointAddress     0x01  EP 1 OUT
+        bmAttributes            2
+          Transfer Type            Bulk
+          Synch Type               None
+          Usage Type               Data
+        wMaxPacketSize     0x0200  1x 512 bytes
+        bInterval               0
+      Endpoint Descriptor:
+        bLength                 7
+        bDescriptorType         5
+        bEndpointAddress     0x81  EP 1 IN
+        bmAttributes            2
+          Transfer Type            Bulk
+          Synch Type               None
+          Usage Type               Data
+        wMaxPacketSize     0x0200  1x 512 bytes
+        bInterval               0
+*/
+#include "descriptors.h"
+
+__code __at(0x3e00) struct usb_descriptors code_descriptors =
+{
+   .device = {
+      .bLength		= USB_DT_DEVICE_SIZE,
+      .bDescriptorType	= USB_DT_DEVICE,
+      .bcdUSB			= USB_BCD_V20,
+      .bDeviceClass 		= USB_CLASS_VENDOR_SPEC,
+      .bDeviceSubClass	= USB_SUBCLASS_VENDOR_SPEC,
+      .bDeviceProtocol	= 0xff,
+      .bMaxPacketSize0	= 64,
+      .idVendor		= 0x0403,
+      .idProduct		= 0x6010,
+      .bcdDevice		= 0x0001,
+      .iManufacturer		= USB_STRING_INDEX(0),
+      .iProduct		= USB_STRING_INDEX(1),
+      .iSerialNumber		= USB_STRING_INDEX_NONE,
+      .bNumConfigurations	= 1,
+   },
+   .qualifier = {
+      .bLength		= USB_DT_DEVICE_QUALIFIER_SIZE,
+      .bDescriptorType	= USB_DT_DEVICE_QUALIFIER,
+      .bcdUSB			= USB_BCD_V20,
+      .bDeviceClass		= USB_CLASS_VENDOR_SPEC,
+      .bDeviceSubClass	= USB_SUBCLASS_VENDOR_SPEC,
+      .bDeviceProtocol	= 0xff,
+      .bMaxPacketSize0	= 64,
+      .bNumConfigurations	= 1,
+      .bRESERVED		= 0,
+   },
+   .highspeed = {
+      .config = {
+         .bLength		= USB_DT_CONFIG_SIZE,
+         .bDescriptorType	= USB_DT_CONFIG,
+         .wTotalLength		= sizeof(descriptors.highspeed),
+         .bNumInterfaces		= 1,
+         .bConfigurationValue	= 1,
+         .iConfiguration		= 0,
+         .bmAttributes		= USB_CONFIG_ATT_ONE,
+         .bMaxPower		= 0x32, // FIXME: ???
+      },
+      .interface = {
+         .bLength		= USB_DT_INTERFACE_SIZE,
+         .bDescriptorType	= USB_DT_INTERFACE,
+         .bInterfaceNumber	= 0,
+         .bAlternateSetting	= 0,
+         .bNumEndpoints		= 2,
+         .bInterfaceClass	= USB_CLASS_VENDOR_SPEC,
+         .bInterfaceSubClass	= USB_SUBCLASS_VENDOR_SPEC,
+         .bInterfaceProtocol	= USB_PROTOCOL_VENDOR_SPEC,
+         .iInterface		= USB_STRING_INDEX(2),
+      },
+      .endpoints = {
+         {
+            .bLength		= USB_DT_ENDPOINT_SIZE,
+            .bDescriptorType 	= USB_DT_ENDPOINT,
+            .bEndpointAddress	= USB_ENDPOINT_NUMBER(0x1) | USB_DIR_OUT,
+            .bmAttributes		= USB_ENDPOINT_XFER_BULK,
+            .wMaxPacketSize		= 512,
+            .bInterval		= 0,
+         },
+         {
+            .bLength		= USB_DT_ENDPOINT_SIZE,
+            .bDescriptorType 	= USB_DT_ENDPOINT,
+            .bEndpointAddress	= USB_ENDPOINT_NUMBER(0x1) | USB_DIR_IN,
+            .bmAttributes		= USB_ENDPOINT_XFER_BULK,
+            .wMaxPacketSize		= 512,
+            .bInterval		= 0,
+         },
+      },
+   },
+   .fullspeed = {
+      .config = {
+         .bLength		= USB_DT_CONFIG_SIZE,
+         .bDescriptorType	= USB_DT_CONFIG,
+         .wTotalLength		= sizeof(descriptors.fullspeed),
+         .bNumInterfaces		= 1,
+         .bConfigurationValue	= 1,
+         .iConfiguration		= 0,
+         .bmAttributes		= USB_CONFIG_ATT_ONE,
+         .bMaxPower		= 0x32, // FIXME: ???
+      },
+      .interface = {
+         .bLength		= USB_DT_INTERFACE_SIZE,
+         .bDescriptorType	= USB_DT_INTERFACE,
+         .bInterfaceNumber	= 0,
+         .bAlternateSetting	= 0,
+         .bNumEndpoints		= 2,
+         .bInterfaceClass	= USB_CLASS_VENDOR_SPEC,
+         .bInterfaceSubClass	= USB_SUBCLASS_VENDOR_SPEC,
+         .bInterfaceProtocol	= 0xff,
+         .iInterface		= 3,
+      },
+      .endpoints = {
+         {
+            .bLength		= USB_DT_ENDPOINT_SIZE,
+            .bDescriptorType 	= USB_DT_ENDPOINT,
+            .bEndpointAddress	= USB_ENDPOINT_NUMBER(0x2) | USB_DIR_OUT,
+            .bmAttributes		= USB_ENDPOINT_XFER_BULK,
+            .wMaxPacketSize		= 64,
+            .bInterval		= 0,
+         },
+         {
+            .bLength		= USB_DT_ENDPOINT_SIZE,
+            .bDescriptorType 	= USB_DT_ENDPOINT,
+            .bEndpointAddress	= USB_ENDPOINT_NUMBER(0x6) | USB_DIR_IN,
+            .bmAttributes		= USB_ENDPOINT_XFER_BULK,
+            .wMaxPacketSize		= 64,
+            .bInterval		= 0,
+         },
+      },
+   },
+#include "build/descriptors_stringtable.inc"
+};
---
+++ examples/loopback_ftdi/ftdi_main.c
@@ -0,0 +1,162 @@
+/**
+ * Copyright (C) 2009 Ubixum, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ **/
+#include <fx2regs.h>
+#include <fx2macros.h>
+#include <autovector.h>
+#include <setupdat.h>
+#include <eputils.h>
+#include <delay.h>
+#include "ftdi_conf.h"
+#ifdef DEBUG_MAIN
+#include <stdio.h> // NOTE this needs deleted
+#else
+#define printf(...)
+#endif
+
+
+//For handling SUDAV ISR
+volatile __bit got_sud;
+
+void main()
+{
+    //Setup data available and other init
+    got_sud=FALSE;
+    //Call our custom function to do our UART init
+    configure_endpoints();
+    RENUMERATE();
+    SETCPUFREQ(CLK_48M);
+    //Enable USB auto vectored interrupts
+    USE_USB_INTS();
+    ENABLE_SUDAV();
+    ENABLE_SOF();
+    ENABLE_HISPEED();
+    ENABLE_USBRESET();
+    EA=1; // global interrupt enable
+    while(TRUE)
+    {
+        //Handles device descriptor requests
+        if ( got_sud )
+        {
+            handle_setupdata();
+            got_sud=FALSE;
+        }
+        // Input data on EP1
+        if(!(EP1OUTCS & bmEPBUSY))
+        {
+            process_recv_data();
+        }
+    }
+}
+
+BOOL
+handle_get_descriptor ()
+{
+    return FALSE;
+}
+
+BOOL
+handle_vendorcommand (BYTE cmd)
+{
+    printf ("Need to implement vendor command: %02x\n", cmd);
+    return FALSE;
+}
+
+// this firmware only supports 0,0
+BOOL
+handle_get_interface (BYTE ifc, BYTE * alt_ifc)
+{
+    printf ("Get Interface\n");
+
+    if (ifc == 0)
+    {
+        *alt_ifc = 0;
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+
+BOOL
+handle_set_interface (BYTE ifc, BYTE alt_ifc)
+{
+    printf ("Set interface %d to alt: %d\n", ifc, alt_ifc);
+
+    if (ifc == 0 && alt_ifc == 0)
+    {
+        // SEE TRM 2.3.7
+        // reset toggles
+        RESETTOGGLE (0x02);
+        RESETTOGGLE (0x86);
+        // restore endpoints to default condition
+        RESETFIFO (0x02);
+        EP2BCL = 0x80;
+        SYNCDELAY;
+        EP2BCL = 0X80;
+        SYNCDELAY;
+        RESETFIFO (0x86);
+        return TRUE;
+    }
+    else
+        return FALSE;
+}
+
+void
+sof_isr ()
+__interrupt SOF_ISR __using 1
+{
+    CLEAR_SOF ();
+}
+
+// get/set configuration
+BYTE
+handle_get_configuration ()
+{
+    return 1;
+}
+
+BOOL
+handle_set_configuration (BYTE cfg)
+{
+    return cfg == 1 ? TRUE : FALSE;	// we only handle cfg 1
+}
+
+// copied usb jt routines from usbjt.h
+void
+sudav_isr ()
+__interrupt SUDAV_ISR
+{
+
+    got_sud = TRUE;
+    CLEAR_SUDAV ();
+}
+
+void usbreset_isr ()
+__interrupt USBRESET_ISR
+{
+    handle_hispeed (FALSE);
+    CLEAR_USBRESET ();
+}
+
+void hispeed_isr ()
+__interrupt HISPEED_ISR
+{
+    handle_hispeed (TRUE);
+    CLEAR_HISPEED ();
+}
