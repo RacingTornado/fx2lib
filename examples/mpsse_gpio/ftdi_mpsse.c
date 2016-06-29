0d96fce0a848a54cb5620df5b9d45709027667ee
Roaming <rangerroaming@gmail.com>
2016-06-29 15:45:30 +0530

    EP2 now receiving

--- examples/mpsse_gpio/ftdi_mpsse.c
+++ examples/mpsse_gpio/ftdi_mpsse.c
@@ -48,6 +48,7 @@ void main()
     ENABLE_HISPEED();
     ENABLE_USBRESET();
     ENABLE_EP2();
+    EP2BCL = 0xff;
     EA=1; // global interrupt enable
 
     while(TRUE)
@@ -167,7 +168,11 @@ __interrupt HISPEED_ISR
 void ep2_isr()
 __interrupt EP2_ISR
 {
+    uart_tx(0x44);
     uart_tx(EP2BCL);
+    uart_tx(0x60);
+    uart_tx(EP2FIFOBUF[0]);
+    EP2BCL = 0xff;
     CLEAR_EP2();
 }
 
